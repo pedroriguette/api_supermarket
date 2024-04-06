ad67267d7b9e37ba311a90ebc288ad4ef1cf6b5b
pedroriguette <pedromedeiros062005@gmail.com>
2024-04-06 18:44:14 -0300

    applying pep8 good practices

--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,11 @@
-from rest_framework import generics
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from project_supermarket.permissions import GlobalDefaultPermissionClass
 from categories.models import Categorie
 from categories.serializers import CategorieSerializer
 from products.models import Product
-from products.serializers import ProductSerializers, ProductRetrieveSerializer
+from products.serializers import ProductRetrieveSerializer
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
